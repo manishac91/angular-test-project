4b75c2f67e564f314dac6065eeda0f42bb0ca138
Manisha <manishac91@gmail.com>
2015-07-05 13:01:49 +0530

    Angular - project: edit and delete features

--- angular-demo/scripts/app.js
+++ angular-demo/scripts/app.js
@@ -5,7 +5,7 @@
 var TodoApp = angular.module("TodoApp", []);
 
 TodoApp.run(function($rootScope){
-	$rootScope.tasks = [];
+	$rootScope.tasks = {};
 })
 
 TodoApp.mainController = function($scope,$rootScope)
@@ -15,14 +15,47 @@ TodoApp.mainController = function($scope,$rootScope)
 	$scope.addTask = function(text)
 	{
 		console.log(text+" task added!!");
-		$rootScope.tasks.push(text);
+		if($rootScope.tasks.data == undefined) {
+			$rootScope.tasks.data = [{
+				id: 1,
+	            title: text,
+	            selected: false,
+	            isEditable: false
+			}];	
+		} else {
+			$rootScope.tasks.data.push({
+            	id: $scope.tasks.data.length + 1,
+            	title: text,
+            	selected: false,
+            	isEditable: false
+        	});	
+		}
 	}
 }
 
 
 TodoApp.tasksController = function($scope, $rootScope)
 {
+	console.log($rootScope.tasks);
+	//$scope.tasks = $rootScope.tasks;
+	$scope.removeTask = function(index){
+		console.log("Remove task");
+		$rootScope.tasks.data.splice(index, 1);
+	},
 
+	$scope.editTask = function(task){
+		if(task.selected){
+			alert('Completed task cannot be edited');
+			return;
+		}
+		task.isEditable = true;
+	},
+
+	$scope.saveTask = function(task){
+		console.log(document.getElementById('input_val_'+task.id));
+		console.log(task);
+		task.isEditable = false;
+	}
 }
 
 TodoApp.controller("mainController", TodoApp.mainController);
